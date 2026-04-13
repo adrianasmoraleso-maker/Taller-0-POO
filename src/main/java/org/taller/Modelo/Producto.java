56e2ef7f53533877a7452d36d677eae29072c595
Adriana Morales <adrianasmoraleso@gmail.com>
2026-04-13 14:13:14 -0500

    chore/Ejercicio15, se le agrego a la clase Producto el metodo vender para disminuir el stock

--- src/main/java/org/taller/Modelo/Producto.java
+++ src/main/java/org/taller/Modelo/Producto.java
@@ -11,5 +11,14 @@ public class Producto{
             System.out.println("Precio: "+ "$" + precio);
             System.out.println("Stock: "+ stock);
         }
+
+    public void vender(int cantidad) {
+        if (cantidad <= stock) {
+            stock -= cantidad;
+            System.out.println("Vendidos " + cantidad + " de " + nombre + ". Stock restante: " + stock);
+        } else {
+            System.out.println("No hay suficiente stock para vender " + cantidad + " unidades.");
+        }
+    }
     }
 
