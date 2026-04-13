fd3b7004e5f17d0212fde305a24cda2fc0501214
Adriana Morales <adrianasmoraleso@gmail.com>
2026-04-13 14:56:47 -0500

    chore/Ejercicio19, se escribió un comentario en el codigo para explocar por qué Estudiante es una clase, y estudiante1 un objeto

--- src/main/java/org/taller/Ejercicios/Ejercicio6.java
+++ src/main/java/org/taller/Ejercicios/Ejercicio6.java
@@ -12,3 +12,18 @@ public class Ejercicio6 {
         estudiante1.mostrarInfo();
     }
 }
+
+// ── Ejercicio 19 ──────────────────────────────────────────────────
+/*
+ * ¿Por qué Estudiante es una clase y estudiante1 es un objeto?
+ *
+ * Estudiante es una clase porque esta es como un molde, o una plantilla, que define
+ * cómo serán todos los estudiantes, es decir, qué atributos tendrán( en este caso: nombre, codigo,
+ * semestre) y qué funciones podrán tener (mostrarInfo, cambiarSemestre).
+ * La clase en sí no representa a ningún estudiante en específico; solo define la estructura.
+ *
+ * estudiante1 es un objeto porque es una instancia concreta creada a partir del molde estudiante.
+ * Tiene valores reales asignados a sus atributos y ocupa espacio en la memoria.
+ * Cuando escribí: Estudiante estudiante1 = new Estudiante();
+ * se creo un objeto individual a partir del "molde" Estudiante.
+ */
