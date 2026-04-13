940f7961dacf77349b0efae369f923ad9e841754
Adriana Morales <adrianasmoraleso@gmail.com>
2026-04-13 17:12:55 -0500

    chore/Ejercicio 22, creacion de clase Libro con 4 objetos distintos a partir de este + conclusion final sobre la clase y los objetos

--- src/main/java/org/taller/Modelo/Libro.java
+++ src/main/java/org/taller/Modelo/Libro.java
@@ -1,4 +1,13 @@
 package org.taller.Modelo;
 
 public class Libro {
+    public String titulo;
+    public String autor;
+    public int paginas;
+
+    public void mostrarInfo() {
+        System.out.println("Título: " + titulo);
+        System.out.println("Autor: " + autor);
+        System.out.println("Páginas: " + paginas);
+    }
 }
