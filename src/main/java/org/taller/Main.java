25e329cdccba3482e5b74920c97cc28e2663532e
Adriana Morales <adrianasmoraleso@gmail.com>
2026-04-13 12:05:48 -0500

    chore/configuracion basica del proyecto

---
+++ src/main/java/org/taller/Main.java
@@ -0,0 +1,7 @@
+package org.taller;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola, esta es una prueba para verificar que todo este funcionando bien :D!");
+    }
+}
