98f8a8375dafbe037a18719e9e3f327635c411be
Adriana Morales <adrianasmoraleso@gmail.com>
2026-04-13 13:29:31 -0500

    chore/Ejercicio10, crear dos objetos de CuentaBancaria que muestren sus propios valores

---
+++ src/main/java/org/taller/Ejercicios/Ejercicio10.java
@@ -0,0 +1,24 @@
+package org.taller.Ejercicios;
+
+import org.taller.Modelo.CuentaBancaria;
+
+public class Ejercicio10 {
+    public static void main(String[] args){
+
+        System.out.println("\nCuenta 1\n");
+        CuentaBancaria cuenta1 = new CuentaBancaria();
+        cuenta1.numero = 801123456;
+        cuenta1.titular = "Ana Pérez";
+        cuenta1.saldo = 1500000;
+
+        cuenta1.mostrarCuenta();
+
+        System.out.println("\nCuenta 2\n");
+        CuentaBancaria   cuenta2 = new CuentaBancaria();
+        cuenta2.numero = 1025123523;
+        cuenta2.titular = "Carlos López";
+        cuenta2.saldo = 320000;
+
+        cuenta2.mostrarCuenta();
+    }
+}
