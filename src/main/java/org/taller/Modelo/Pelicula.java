f0bfb2986ed52af26419d5d447f6df7b92fff378
Adriana Morales <adrianasmoraleso@gmail.com>
2026-04-14 10:38:12 -0500

    chore/Ejercicio73: se reescribio el ejercicio 40 de la clase Pelicula ahora usando un constructor copia

--- src/main/java/org/taller/Modelo/Pelicula.java
+++ src/main/java/org/taller/Modelo/Pelicula.java
@@ -1,5 +1,37 @@
 package org.taller.Modelo;
 
+//Ejercicio 73: reescribir una clase usando constructor copia
+
+public class Pelicula {
+    public String nombre;
+    public String genero;
+    public int minutos;
+
+    // Constructor normal
+    public Pelicula(String nombre, String genero, int minutos) {
+        this.nombre = nombre;
+        this.genero = genero;
+        this.minutos = minutos;
+    }
+
+    // Constructor copia
+    public Pelicula(Pelicula otra) {
+        this.nombre = otra.nombre;
+        this.genero = otra.genero;
+        this.minutos = otra.minutos;
+    }
+
+    public void mostrarFicha() {
+        System.out.println("Película: " + nombre);
+        System.out.println("Género: " + genero);
+        System.out.println("Duración: " + minutos + " min");
+    }
+}
+
+//Código original
+
+/*
+
 public class Pelicula {
     public String nombre;
     public String genero;
@@ -11,3 +43,4 @@ public class Pelicula {
         System.out.println("Duración: " + minutos + " min");
     }
 }
+*/
