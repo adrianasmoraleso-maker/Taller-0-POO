eecd300c78bf121f0f117451d087bae2bc476c4d
Adriana Morales <adrianasmoraleso@gmail.com>
2026-04-14 10:26:43 -0500

    chore/Ejercicio71: se reescribio el ejercicio 35 de la clase Lampara ahora usando un constructor con parametos

--- src/main/java/org/taller/Modelo/Lampara.java
+++ src/main/java/org/taller/Modelo/Lampara.java
@@ -1,5 +1,36 @@
 package org.taller.Modelo;
 
+//Ejercicio 71: reescribir uno de los primeros ejercicios usando constructor cn parámetros
+
+public class Lampara {
+    public String marca;
+    public boolean encendida;
+
+    // Constructor con parámetros
+    public Lampara(String marca, boolean encendida) {
+        this.marca = marca;
+        this.encendida = encendida;
+    }
+
+    public void encender() {
+        encendida = true;
+        System.out.println("Lámpara " + marca + " encendida.");
+    }
+
+    public void apagar() {
+        encendida = false;
+        System.out.println("Lámpara " + marca + " apagada.");
+    }
+
+    public void mostrarEstado() {
+        System.out.println("Lámpara: " + marca);
+        System.out.println("Estado: " + (encendida ? "Encendida" : "Apagada"));
+    }
+}
+
+// Código original
+
+/*
 public class Lampara {
     public String marca;
     public boolean encendida;
@@ -20,3 +51,4 @@ public class Lampara {
     }
 }
 
+*/
